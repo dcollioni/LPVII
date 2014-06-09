95fadb3d1c2bd80aed56a9c30c330d6f4a8cb3ba
Douglas Collioni <dcollioni@gmail.com>
2014-06-09 08:57:39 -0300

    Projeto CadastroJogador.

---
+++ CadastroJogador/src/cadastrojogador/CadastroJogador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cadastrojogador;
+
+/**
+ *
+ * @author Professor
+ */
+public class CadastroJogador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CadastroJogadorView view = new CadastroJogadorView();
+        view.setVisible(true);
+    }
+    
+}
---
+++ CadastroJogador/src/cadastrojogador/Jogador.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cadastrojogador;
+
+/**
+ *
+ * @author Professor
+ */
+public class Jogador {
+    private String nome;
+    private String pais;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
